4548a4fb7be3e440829bb4f4bd958287b8c58c27
Tamera Lanham <tamera.lanham@gmail.com>
2022-06-17 23:23:54 -0700

    refactor: create HyperparamsBase

--- mega_experiment/training_jobs/example_training_job.py
+++ mega_experiment/training_jobs/example_training_job.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from inspect import Parameter
 from pyexpat import model
-from mega_experiment.training_jobs.training_job import TrainingJob, create_job_hyperparams_class, HyperparamOptions
+from mega_experiment.training_jobs.training_job import TrainingJob, create_job_hyperparams_class, HyperparamsBase
 import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader, Dataset
 from typing import Iterable, Union
 
 @dataclass
-class Hyperparams:
+class Hyperparams(HyperparamsBase):
     n_inputs: int = 16
     batch_size: int = 32
     n_batches: int = 500
@@ -32,7 +32,7 @@ class IdentityDataset(Dataset):
 
 class ExampleTrainingJob(TrainingJob):
 
-    def model(self, hyperparams: Hyperparams) -> t.Module:
+    def model(self, hyperparams: Hyperparams) -> t.nn.Module:
         input_size = hyperparams.n_inputs
         hidden_size = hyperparams.n_inputs * 4
         output_size = hyperparams.n_inputs
@@ -59,10 +59,10 @@ class ExampleTrainingJob(TrainingJob):
         )
         return train_loader, val_loader
 
-    def optimizer(self, hyperparams: Hyperparams, model: t.Module) -> t.optim.Optimizer:
+    def optimizer(self, hyperparams: Hyperparams, model: t.nn.Module) -> t.optim.Optimizer:
         return t.optim.SGD(model.parameters(), lr=hyperparams.lr)
 
-    def train_step(self, hyperparams: object, model: t.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+    def train_step(self, hyperparams: Hyperparams, model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
         return self.get_loss_and_y_pred(model, batch, F.mse_loss)
 
 
--- mega_experiment/training_jobs/training_job.py
+++ mega_experiment/training_jobs/training_job.py
@@ -5,6 +5,8 @@
 # - Should there be different metadata used to construct the model for torchscripting? Should the model just be torchscripted on each init?
 
 from dataclasses import dataclass, fields, make_dataclass
+from datetime import datetime
+from pathlib import Path
 import subprocess
 import torch as t
 import torch.nn.functional as F
@@ -16,41 +18,41 @@ from typing import Optional, Union, TypeVar
 # encouraged to overwrite these methods to suit their needs.
 
 class TrainingJob:
-    def __init__(self, training_job_settings: 'TrainingJobSettings', job_hyperparams: object):
-        self.settings = training_job_settings
-        self.job_hyperparams = job_hyperparams
 
-    def model(self, hyperparams: object) -> t.Module:
+    def model(self, hyperparams: 'HyperparamsBase') -> t.nn.Module:
         raise NotImplementedError('Subclasses should implement this method.')
 
-    def data_loaders(self, hyperparams: object) -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
+    def data_loaders(self, hyperparams: 'HyperparamsBase') -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
         raise NotImplementedError('Subclasses should implement this method.')
 
-    def optimizer(self, hyperparams: object, model: t.Module) -> t.optim.Optimizer:
+    def optimizer(self, hyperparams: 'HyperparamsBase', model: t.nn.Module) -> t.optim.Optimizer:
         return t.optim.SGD(model.parameters())
 
-    def get_loss_and_y_pred(self, model: t.Module, batch: tuple[t.Tensor, t.Tensor], loss_fn: callable) -> tuple[t.Tensor, t.Tensor]:
+    def get_loss_and_y_pred(self, model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor], loss_fn: callable) -> tuple[t.Tensor, t.Tensor]:
         X, y = batch
         y_pred = model(X)
         loss = loss_fn(y_pred, y)
         return loss, y_pred
 
-    def train_step(self, hyperparams: object, model: t.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+    def train_step(self, hyperparams: 'HyperparamsBase', model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
         return self.get_loss_and_y_pred(model, batch, F.cross_entropy)
 
-    def validation_step(self, hyperparams: object, model: t.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+    def validation_step(self, hyperparams: 'HyperparamsBase', model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
         return self.train_step(hyperparams, model, batch)
 
 @dataclass
 class TrainingJobSettings:
+    job_type: str = "TrainingJob"
     job_description: str = ""
     n_init_repeats: int = 1
     gcs_bucket: Optional[str] = None
     n_processes: int = 4
+    output_dirname: str = ""
     most_recent_commit_hash: str = ""
 
     def __post_init__(self):
         self.most_recent_commit_hash = self._current_commit_hash()
+        if not self.output_dirname: self.output_dirname = self._output_dirname()
 
     def _current_commit_hash(self) -> str:
         return (
@@ -59,6 +61,13 @@ class TrainingJobSettings:
             .strip()
         )
 
+    def _output_dirname(self) -> str:
+        timestamp = datetime.now().strftime("%Y-%m-%d--%H-%M")
+        return self.job_type + "_" + timestamp
+
+@dataclass
+class HyperparamsBase:
+    instance_id: Optional[int] = None
 
 class HyperparamOptions(list[TypeVar("T")]):
     def __init__(self, *args, **kwargs):
