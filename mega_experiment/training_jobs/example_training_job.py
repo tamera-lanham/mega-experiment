f01665cc5b03a5eaf8fd8f625e9413bffd60b003
Tamera Lanham <tamera.lanham@gmail.com>
2022-06-17 22:03:20 -0700

    feat: TrainingJob

---
+++ mega_experiment/training_jobs/example_training_job.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+from inspect import Parameter
+from pyexpat import model
+from mega_experiment.training_jobs.training_job import TrainingJob, create_job_hyperparams_class, HyperparamOptions
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from typing import Iterable, Union
+
+@dataclass
+class Hyperparams:
+    n_inputs: int = 16
+    batch_size: int = 32
+    n_batches: int = 500
+    epochs: int = 5
+    lr: float = 1e-3
+
+JobHyperparams = create_job_hyperparams_class(Hyperparams)
+
+
+class IdentityDataset(Dataset):
+    def __init__(self, shape, n_batches):
+        self.data = [t.randn(shape) for _ in range(n_batches)]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        X = self.data[idx]
+        return (X, X)
+
+class ExampleTrainingJob(TrainingJob):
+
+    def model(self, hyperparams: Hyperparams) -> t.Module:
+        input_size = hyperparams.n_inputs
+        hidden_size = hyperparams.n_inputs * 4
+        output_size = hyperparams.n_inputs
+
+        model = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+        )
+
+        return model
+
+    def data_loaders(self, hyperparams: Hyperparams) -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
+        
+        train_loader = DataLoader(
+            IdentityDataset(
+                (hyperparams.batch_size, hyperparams.n_inputs), hyperparams.n_batches
+            )
+        )
+        val_loader = DataLoader(
+            IdentityDataset(
+                (hyperparams.batch_size, hyperparams.n_inputs), hyperparams.n_batches // 5
+            )
+        )
+        return train_loader, val_loader
+
+    def optimizer(self, hyperparams: Hyperparams, model: t.Module) -> t.optim.Optimizer:
+        return t.optim.SGD(model.parameters(), lr=hyperparams.lr)
+
+    def train_step(self, hyperparams: object, model: t.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+        return self.get_loss_and_y_pred(model, batch, F.mse_loss)
+
+
+
+
--- mega_experiment/training_jobs/training_job.py
+++ mega_experiment/training_jobs/training_job.py
@@ -5,16 +5,59 @@
 # - Should there be different metadata used to construct the model for torchscripting? Should the model just be torchscripted on each init?
 
 from dataclasses import dataclass, fields, make_dataclass
+import subprocess
+import torch as t
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from typing import Optional, Union, TypeVar
 
+
+# All methods on this class are overwriteable by subclasses! In fact, subclasses are 
+# encouraged to overwrite these methods to suit their needs.
+
+class TrainingJob:
+    def __init__(self, training_job_settings: 'TrainingJobSettings', job_hyperparams: object):
+        self.settings = training_job_settings
+        self.job_hyperparams = job_hyperparams
+
+    def model(self, hyperparams: object) -> t.Module:
+        raise NotImplementedError('Subclasses should implement this method.')
+
+    def data_loaders(self, hyperparams: object) -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
+        raise NotImplementedError('Subclasses should implement this method.')
+
+    def optimizer(self, hyperparams: object, model: t.Module) -> t.optim.Optimizer:
+        return t.optim.SGD(model.parameters())
+
+    def get_loss_and_y_pred(self, model: t.Module, batch: tuple[t.Tensor, t.Tensor], loss_fn: callable) -> tuple[t.Tensor, t.Tensor]:
+        X, y = batch
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        return loss, y_pred
+
+    def train_step(self, hyperparams: object, model: t.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+        return self.get_loss_and_y_pred(model, batch, F.cross_entropy)
+
+    def validation_step(self, hyperparams: object, model: t.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+        return self.train_step(hyperparams, model, batch)
+
 @dataclass
-class TheseShouldProbablyBeConstructorArgsForATrainingJob:
-    job_type: str = "TrainingJob"
-    description: str = ""
+class TrainingJobSettings:
+    job_description: str = ""
     n_init_repeats: int = 1
     gcs_bucket: Optional[str] = None
     n_processes: int = 4
+    most_recent_commit_hash: str = ""
 
+    def __post_init__(self):
+        self.most_recent_commit_hash = self._current_commit_hash()
+
+    def _current_commit_hash(self) -> str:
+        return (
+            subprocess.check_output(["git", "rev-parse", "HEAD"])
+            .decode("ascii")
+            .strip()
+        )
 
 
 class HyperparamOptions(list[TypeVar("T")]):
@@ -24,13 +67,8 @@ class HyperparamOptions(list[TypeVar("T")]):
 def create_job_hyperparams_class(hyperparams_class: type):
     """
     Create a new dataclass based on an original, where the new class is the same as the original
-    but with a HyperparamOptions type hint option on each of the attibutes.
+    but with a HyperparamOptions type hint option added to each of the attibutes.
     """
-
     enhance_type = lambda type_: Union[type_, HyperparamOptions[type_]]
-
-    job_metadata_class_fields = []
-    for field in fields(hyperparams_class):
-        job_metadata_class_fields.append((field.name, enhance_type(field.type), field))
-
-    return make_dataclass('JobHyperparams', job_metadata_class_fields)
+    job_hyperparams_class_fields = [(field.name, enhance_type(field.type), field) for field in fields(hyperparams_class)]
+    return make_dataclass('JobHyperparams', job_hyperparams_class_fields)
