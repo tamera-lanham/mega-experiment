4dabfc5c6e59b57856ea9312909a2d0bbb4ec337
Tamera Lanham <tamera.lanham@gmail.com>
2022-06-17 23:24:38 -0700

    feat: instance runner and test pretraining setup

--- mega_experiment/job_runner/instance_runner.py
+++ mega_experiment/job_runner/instance_runner.py
@@ -5,3 +5,39 @@
 # - Saving the params, metrics, val outputs and such while training
 # - Saving to GCP
 # - Reporting progress in the console??
+
+from dataclasses import asdict
+from mega_experiment.training_jobs.training_job import TrainingJob, HyperparamsBase
+import json
+import os
+from pathlib import Path
+import torch as t
+
+
+class InstanceRunner:
+
+    def __init__(self, training_job: TrainingJob, hyperparams: HyperparamsBase, job_output_dir: Path):
+        self.training_job = training_job
+        self.job_ouput_dir = job_output_dir
+        self.hyperparams = hyperparams
+
+    def run(self):
+        self.pre_training_setup()
+        # Train
+        # Save the params, metrics, val outputs and such while training
+        # Save to GCP
+
+    def pre_training_setup(self):
+        # Make the output dir
+        output_dir = self.job_ouput_dir / "instances" / str(self.hyperparams.instance_id)
+        os.makedirs(output_dir, exist_ok=True)
+
+        # Save hyperparams
+        with open(self.job_ouput_dir / "instance_metadata.jsonl", "a") as f:
+            f.write(json.dumps(asdict(self.hyperparams)) + "\n")
+
+        # Trace and save model
+        model = self.training_job.model(self.hyperparams)
+        scripted_model = t.jit.script(model)
+        t.jit.save(scripted_model, output_dir / "model_torchscript.pt")
+
---
+++ mega_experiment/job_runner/instance_runner_test.py
@@ -0,0 +1,21 @@
+from mega_experiment.job_runner.instance_runner import *
+from mega_experiment.training_jobs.example_training_job import ExampleTrainingJob, Hyperparams
+import os
+from pathlib import Path
+import shutil
+import torch as t
+
+
+def test_pre_training_setup():
+    training_job = ExampleTrainingJob()
+    hyperparams = Hyperparams(instance_id=42)
+    base_output_dir = Path('outputs/instance-runner-test')
+
+    instance_runner = InstanceRunner(training_job, hyperparams, base_output_dir)
+    instance_runner.pre_training_setup()
+
+    assert (base_output_dir / 'instances' / str(hyperparams.instance_id)).is_dir()
+    assert (base_output_dir / 'instances' / str(hyperparams.instance_id) / 'model_torchscript.pt').is_file()
+    assert (base_output_dir / 'instance_metadata.jsonl').is_file()
+
+    shutil.rmtree(base_output_dir)
