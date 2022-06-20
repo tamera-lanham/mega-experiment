505f27feb4fde90595b7668bd58a0afc31350181
Tamera Lanham <tamera.lanham@gmail.com>
2022-06-20 11:26:13 -0700

    style: black formatting

--- mega_experiment/training_jobs/example_training_job.py
+++ mega_experiment/training_jobs/example_training_job.py
@@ -1,20 +1,26 @@
 from dataclasses import dataclass
 from inspect import Parameter
 from pyexpat import model
-from mega_experiment.training_jobs.training_job import TrainingJob, create_job_hyperparams_class, HyperparamsBase
+from mega_experiment.training_jobs.training_job import (
+    TrainingJob,
+    create_job_hyperparams_class,
+    HyperparamsBase,
+)
 import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
-from typing import Iterable, Union
+from typing import Iterable, Optional, Union
+
 
 @dataclass
 class Hyperparams(HyperparamsBase):
     n_inputs: int = 16
     batch_size: int = 32
     n_batches: int = 500
-    epochs: int = 5
+    n_epochs: int = 5
     lr: float = 1e-3
+
 
 JobHyperparams = create_job_hyperparams_class(Hyperparams)
 
@@ -30,8 +36,8 @@ class IdentityDataset(Dataset):
         X = self.data[idx]
         return (X, X)
 
-class ExampleTrainingJob(TrainingJob):
 
+class ExampleTrainingJob(TrainingJob):
     def model(self, hyperparams: Hyperparams) -> t.nn.Module:
         input_size = hyperparams.n_inputs
         hidden_size = hyperparams.n_inputs * 4
@@ -45,16 +51,15 @@ class ExampleTrainingJob(TrainingJob):
 
         return model
 
-    def data_loaders(self, hyperparams: Hyperparams) -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
-        
+    def data_loaders(self, hyperparams: Hyperparams) -> tuple[DataLoader, Optional[DataLoader]]:
+
         train_loader = DataLoader(
-            IdentityDataset(
-                (hyperparams.batch_size, hyperparams.n_inputs), hyperparams.n_batches
-            )
+            IdentityDataset((hyperparams.batch_size, hyperparams.n_inputs), hyperparams.n_batches)
         )
         val_loader = DataLoader(
             IdentityDataset(
-                (hyperparams.batch_size, hyperparams.n_inputs), hyperparams.n_batches // 5
+                (hyperparams.batch_size, hyperparams.n_inputs),
+                hyperparams.n_batches // 5,
             )
         )
         return train_loader, val_loader
@@ -62,9 +67,15 @@ class ExampleTrainingJob(TrainingJob):
     def optimizer(self, hyperparams: Hyperparams, model: t.nn.Module) -> t.optim.Optimizer:
         return t.optim.SGD(model.parameters(), lr=hyperparams.lr)
 
-    def train_step(self, hyperparams: Hyperparams, model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+    def train_step(
+        self,
+        hyperparams: Hyperparams,
+        model: t.nn.Module,
+        batch: tuple[t.Tensor, t.Tensor],
+    ) -> tuple[t.Tensor, t.Tensor]:
         return self.get_loss_and_y_pred(model, batch, F.mse_loss)
 
-
-
-
+    # Interface to stopping condition
+    def stop_condition(self, hyperparams: Hyperparams, epoch: int, all_metrics: list[dict[str, object]]) -> bool:
+        if hyperparams.n_epochs == epoch:
+            return True
--- mega_experiment/training_jobs/training_job.py
+++ mega_experiment/training_jobs/training_job.py
@@ -14,31 +14,44 @@ from torch.utils.data import DataLoader
 from typing import Optional, Union, TypeVar
 
 
-# All methods on this class are overwriteable by subclasses! In fact, subclasses are 
+# All methods on this class are overwriteable by subclasses! In fact, subclasses are
 # encouraged to overwrite these methods to suit their needs.
 
+
 class TrainingJob:
+    def model(self, hyperparams: "HyperparamsBase") -> t.nn.Module:
+        raise NotImplementedError("Subclasses should implement this method.")
 
-    def model(self, hyperparams: 'HyperparamsBase') -> t.nn.Module:
-        raise NotImplementedError('Subclasses should implement this method.')
+    def data_loaders(self, hyperparams: "HyperparamsBase") -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
+        raise NotImplementedError("Subclasses should implement this method.")
 
-    def data_loaders(self, hyperparams: 'HyperparamsBase') -> Union[DataLoader, tuple[DataLoader, DataLoader]]:
-        raise NotImplementedError('Subclasses should implement this method.')
-
-    def optimizer(self, hyperparams: 'HyperparamsBase', model: t.nn.Module) -> t.optim.Optimizer:
+    def optimizer(self, hyperparams: "HyperparamsBase", model: t.nn.Module) -> t.optim.Optimizer:
         return t.optim.SGD(model.parameters())
 
-    def get_loss_and_y_pred(self, model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor], loss_fn: callable) -> tuple[t.Tensor, t.Tensor]:
+    def get_loss_and_y_pred(
+        self, model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor], loss_fn: callable
+    ) -> tuple[t.Tensor, t.Tensor]:
         X, y = batch
         y_pred = model(X)
         loss = loss_fn(y_pred, y)
         return loss, y_pred
 
-    def train_step(self, hyperparams: 'HyperparamsBase', model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+    def train_step(
+        self,
+        hyperparams: "HyperparamsBase",
+        model: t.nn.Module,
+        batch: tuple[t.Tensor, t.Tensor],
+    ) -> tuple[t.Tensor, t.Tensor]:
         return self.get_loss_and_y_pred(model, batch, F.cross_entropy)
 
-    def validation_step(self, hyperparams: 'HyperparamsBase', model: t.nn.Module, batch: tuple[t.Tensor, t.Tensor]) -> tuple[t.Tensor, t.Tensor]:
+    def validation_step(
+        self,
+        hyperparams: "HyperparamsBase",
+        model: t.nn.Module,
+        batch: tuple[t.Tensor, t.Tensor],
+    ) -> tuple[t.Tensor, t.Tensor]:
         return self.train_step(hyperparams, model, batch)
+
 
 @dataclass
 class TrainingJobSettings:
@@ -52,26 +65,26 @@ class TrainingJobSettings:
 
     def __post_init__(self):
         self.most_recent_commit_hash = self._current_commit_hash()
-        if not self.output_dirname: self.output_dirname = self._output_dirname()
+        if not self.output_dirname:
+            self.output_dirname = self._output_dirname()
 
     def _current_commit_hash(self) -> str:
-        return (
-            subprocess.check_output(["git", "rev-parse", "HEAD"])
-            .decode("ascii")
-            .strip()
-        )
+        return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 
     def _output_dirname(self) -> str:
         timestamp = datetime.now().strftime("%Y-%m-%d--%H-%M")
         return self.job_type + "_" + timestamp
 
+
 @dataclass
 class HyperparamsBase:
     instance_id: Optional[int] = None
 
+
 class HyperparamOptions(list[TypeVar("T")]):
     def __init__(self, *args, **kwargs):
         super().__init__(args[0])
+
 
 def create_job_hyperparams_class(hyperparams_class: type):
     """
@@ -79,5 +92,7 @@ def create_job_hyperparams_class(hyperparams_class: type):
     but with a HyperparamOptions type hint option added to each of the attibutes.
     """
     enhance_type = lambda type_: Union[type_, HyperparamOptions[type_]]
-    job_hyperparams_class_fields = [(field.name, enhance_type(field.type), field) for field in fields(hyperparams_class)]
-    return make_dataclass('JobHyperparams', job_hyperparams_class_fields)
+    job_hyperparams_class_fields = [
+        (field.name, enhance_type(field.type), field) for field in fields(hyperparams_class)
+    ]
+    return make_dataclass("JobHyperparams", job_hyperparams_class_fields)
