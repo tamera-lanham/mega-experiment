34dafe1ca33c3563861da4e765f0762afc2470f0
Tamera Lanham <tamera.lanham@gmail.com>
2022-06-20 15:12:51 -0700

    feat: generate instance hyperparams from hyperparam options

--- mega_experiment/job_runner/job_runner.py
+++ mega_experiment/job_runner/job_runner.py
@@ -1,12 +1,11 @@
-# In charge of
-# - Saving job settings and JobHyperparams
-# - Generating hyperparam instances from JobHyperparams
-# - Parallelizing calls to InstanceRunner
 from dataclasses import asdict
-from typing import Iterable, Type
+from itertools import product
+import json
+import os
 from mega_experiment.job_runner.instance_runner import InstanceRunner
-from mega_experiment.training_jobs.training_job import HyperparamsBase, TrainingJob
+from mega_experiment.training_jobs.training_job import HyperparamOptions, HyperparamsBase, TrainingJob
 from pathlib import Path
+from typing import Iterable
 
 
 class JobRunner:
@@ -14,22 +13,38 @@ class JobRunner:
         self.training_job = training_job
         self.settings = self.training_job.settings
         self.job_hyperparams = job_hyperparams
-        self.Hyperparams = self.job_hyperparams.hyperparams_class
 
         self.job_ouput_dir = Path("outputs") / self.settings.output_dirname
 
     def run(self):
-        # Save training_job.settings
-        # Save job_hyperparams
+        self.save_settings_and_job_hyperparams()
+
         for instance_hyperparams in self.generate_instance_hyperparams():
             self.run_instance(instance_hyperparams)
 
-    def run_instance(self, hyperparams):
+    def save_settings_and_job_hyperparams(self):
+        os.makedirs(self.job_ouput_dir, exist_ok=True)  # TODO: remove exists ok
+        with open(self.job_ouput_dir / "job_settings.json", "w") as f:
+            json.dump(asdict(self.settings), f)
+        with open(self.job_ouput_dir / "job_hyperparams.json", "w") as f:
+            json.dump(asdict(self.job_hyperparams), f)
 
+    def run_instance(self, hyperparams):
         instance_runner = InstanceRunner(self.training_job, hyperparams, self.job_ouput_dir)
         instance_runner.run()
 
     def generate_instance_hyperparams(self) -> Iterable[HyperparamsBase]:
-        # TODO: the cartesian product thing
-        for i in range(self.settings.n_init_repeats):
-            yield self.Hyperparams(instance_id=i, **asdict(self.job_hyperparams))
+
+        hyperparams_with_options = {
+            k: v for k, v in asdict(self.job_hyperparams).items() if isinstance(v, HyperparamOptions)
+        }
+        as_tuples = [[(label, value) for value in options] for label, options in hyperparams_with_options.items()]
+        instance_hyperparam_values = (
+            {**asdict(self.job_hyperparams), **dict(hyperparam_values)} for hyperparam_values in product(*as_tuples)
+        )
+
+        i = 0
+        for instance_hyperparams in instance_hyperparam_values:
+            for _ in range(self.settings.n_instance_repeats):
+                yield self.job_hyperparams.hyperparams_class(instance_id=i, **instance_hyperparams)
+                i += 1
--- scripts/run_example_job.py
+++ scripts/run_example_job.py
@@ -1,5 +1,5 @@
 from mega_experiment.training_jobs.example_training_job import *
-from mega_experiment.training_jobs.training_job import TrainingJobSettings
+from mega_experiment.training_jobs.training_job import HyperparamOptions, TrainingJobSettings
 from mega_experiment.job_runner import job_runner
 
 if __name__ == "__main__":
@@ -7,11 +7,19 @@ if __name__ == "__main__":
         **{
             "job_type": "ExampleTrainingJob",
             "job_description": "Example training job for demo and testing purposes",
-            "n_init_repeats": 5,
+            "n_instance_repeats": 2,
         }
     )
 
-    job_hyperparams = JobHyperparams(**{"n_inputs": 16, "batch_size": 32, "n_batches": 500, "n_epochs": 5, "lr": 0.001})
+    job_hyperparams = JobHyperparams(
+        **{
+            "n_inputs": HyperparamOptions([8, 16, 32]),
+            "batch_size": 32,
+            "n_batches": 500,
+            "n_epochs": 5,
+            "lr": HyperparamOptions([1e-3, 1e-5]),
+        }
+    )
 
     training_job = ExampleTrainingJob(settings)
     job_runner = job_runner.JobRunner(training_job, job_hyperparams)
